701153a74775dbc82ae4b42aa6ede471064eaa37
Kartik Raiyani <93818535+kartikraiyani03@users.noreply.github.com>
2024-02-15 18:44:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.text',
+{
+    strings : ['JAVA Programmer','DSA Learner','Full Stack Web Developer'],
+    typeSpeed : 40, 
+    backSpeed : 40, 
+    backDelay : 1000,
+    loop : true 
+});
